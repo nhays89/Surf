a19357c5470f92502457af94613a68191c853184
nicholas hays <nicholas.hays@hotmail.com>
2016-06-29 22:47:17 -0700

    initial commit

---
+++ Surf/src/SurfMain.java
@@ -0,0 +1,49 @@
+
+public class SurfMain {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	private class Wave {
+		private int myStartTime;
+		private int myFunPoints;
+		private int myWaitTime;
+
+		private int myEndTime;
+		
+		public Wave(int startTime, int funPoints, int waitTime) {
+			myStartTime = startTime;
+			myFunPoints = funPoints;
+			myWaitTime = waitTime;
+			
+			myEndTime = startTime + waitTime;
+		}
+		
+		public boolean isOverlapping(Wave theOtherWave) {
+			
+			return true;
+		}
+
+		
+		
+		
+		public int getMyStartTime() {
+			return myStartTime;
+		}
+
+		public int getMyFunPoints() {
+			return myFunPoints;
+		}
+
+		public int getMyWaitTime() {
+			return myWaitTime;
+		}
+
+		public int getMyEndTime() {
+			return myEndTime;
+		}
+		
+		
+	}
+}
