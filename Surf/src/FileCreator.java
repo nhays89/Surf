17d13a737e6b8b95ca6c72921f6ecebd7e626ea0
nicholas hays <nicholas.hays@hotmail.com>
2016-07-03 00:18:15 -0700

    added file input, commented code, tidied up syso.

---
+++ Surf/src/FileCreator.java
@@ -0,0 +1,46 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FileCreator {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("input_long.txt");
+			fw.write(100000 + "" + '\n');
+			Random r = new Random();
+			ArrayList<int[]> array = new ArrayList<int[]>();
+			for (int i = 0; i < 100000; i++) {
+				int[] arr = new int[] {i, r.nextInt(100000), r.nextInt(100) };
+				//System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
+				array.add(arr);
+			}
+			shuffle(array);
+			
+			for (int i = 0; i < array.size(); i++) {
+				// System.out.println(array.get(i)[0] + " " + array.get(i)[1] + " " + array.get(i)[2]);
+				String stuff = array.get(i)[0] + " " + array.get(i)[1] + " " + array.get(i)[2] + '\n';
+				fw.write(stuff);
+			}
+			
+			/*for (int[] wave : array) {
+				fw.write(wave[0] + " " + wave[1] + " " + wave[2]);
+			}*/
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void shuffle(ArrayList<int[]> theList) {
+		Random r = new Random();
+		for (int i = theList.size() - 1; i > 0; i--) {
+			int index = r.nextInt(i + 1);
+			int[] temp = new int[]{theList.get(index)[0], theList.get(index)[1], theList.get(index)[2]};
+			theList.set(index, theList.get(i));
+			theList.set(i, temp);
+		}
+	}
+}
--- Surf/src/SurfMain.java
+++ Surf/src/SurfMain.java
@@ -1,44 +1,39 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/**
+ * Authors: Nicholas A. Hays & Ethan Rowell
+ */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 /**
+ * Kicks off main program. Input comes from piped input file. 
  * 
- * @author Nicholas A. Hays
+ * @author Nicholas A. Hays & Ethan Rowell
  */
 public class SurfMain {
 	static List<Wave> myWaves = new ArrayList<Wave>();
+	static int numOfWaves;
 
-	@SuppressWarnings("unchecked")
+	/**
+	 * Main method to kick off program. Accepts piped file input.
+	 * Refer to github for input syntax. 
+	 * 
+	 * @param cmd line args not accpeted
+	 */
 	public static void main(String[] args) {
-		try {
-			System.setIn(new FileInputStream("input3.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Scanner scanIn = new Scanner(System.in);
-		System.out.println("Before nextInt");
-		int numOfWaves = scanIn.nextInt();
-		System.out.println("Num waves: " + numOfWaves);
+		Scanner scan = new Scanner(System.in);
+		numOfWaves = scan.nextInt();
 		for (int i = 0; i < numOfWaves; i++) {
-			Wave wave = new Wave(scanIn.nextInt(), scanIn.nextInt(), scanIn.nextInt());
-			System.out.println("Wave: " + wave.myStartTime + ", " + wave.myFunPts + ", " + wave.myWaitTime);
-			myWaves.add(wave);
+			Wave wave = new Wave(scan.nextInt(), scan.nextInt(), scan.nextInt());
+			myWaves.add(i, wave);
+			//System.out.println(
+				//	"start time: " + wave.myStartTime + " points: " + wave.myFunPts + " duration " + wave.myEndTime);
 		}
+		scan.close();
 		Collections.sort(myWaves);
-		scanIn.close();
 		
-		System.out.println("Sorted...");
-		for(Wave wave : myWaves) {
-			System.out.println("Wave: " + wave.myStartTime + ", " + wave.myFunPts + ", " + wave.myWaitTime + ", " + wave.myEndTime);
-		}
-		
+		System.out.println("sorted");
 
 		for (int i = 0; i < numOfWaves; i++) {
 			Wave currentWave = myWaves.get(i);
@@ -46,9 +41,7 @@ public class SurfMain {
 				currentWave.setTotMaxFunPts(myWaves.get(i).myFunPts);
 				currentWave.setActualMaxFunPoints(myWaves.get(i).myFunPts);
 			} else {
-				
 				Wave nonOverlappingWave;
-				
 				if ((nonOverlappingWave = largestNonOverlapping(i)) != null) {
 					currentWave.setActualMaxFunPoints(currentWave.myFunPts + nonOverlappingWave.myActualMaxFunPts);
 					if (currentWave.myActualMaxFunPts > myWaves.get(i - 1).myTotMaxFunPts) {
@@ -57,7 +50,6 @@ public class SurfMain {
 						currentWave.setTotMaxFunPts(myWaves.get(i - 1).myTotMaxFunPts);
 					}
 				} else {
-					
 					currentWave.setActualMaxFunPoints(currentWave.myFunPts);
 					if (currentWave.myActualMaxFunPts > myWaves.get(i - 1).myTotMaxFunPts) {
 						currentWave.setTotMaxFunPts(currentWave.myActualMaxFunPts);
@@ -65,80 +57,80 @@ public class SurfMain {
 						currentWave.setTotMaxFunPts(myWaves.get(i - 1).myTotMaxFunPts);
 					}
 				}
-				
-				
 			}
 		}
-		for(Wave wave : myWaves) {
-			System.out.println("my actual " + wave.myActualMaxFunPts + ", mytotal: " + wave.myTotMaxFunPts);
-		}
-		System.out.println(myWaves.get(myWaves.size() - 1).myTotMaxFunPts
-				);
-		
+		System.out.println(myWaves.get(myWaves.size() - 1).myTotMaxFunPts);
 
 	}
+	
+	/**
+	 * Finds the largest non overlapping wave that ends before this wave begins. 
+	 * 
+	 * @param theWave the wave index in the set of waves.  
+	 * @return the largest non overlapping wave that ends before the  
+	 */
 	public static Wave largestNonOverlapping(int theWave) {
 		Wave currWave = myWaves.get(theWave);
 		int largestActual = 0, indexOfActual = -1;
-		for( int i = theWave - 1; i >= 0; i--) {
+		for (int i = theWave - 1; i >= 0; i--) {
 			if (myWaves.get(i).myEndTime <= currWave.myStartTime && myWaves.get(i).myActualMaxFunPts > largestActual) {
 				largestActual = myWaves.get(i).myActualMaxFunPts;
 				indexOfActual = i;
 			}
 		}
-		if (indexOfActual != -1) return myWaves.get(indexOfActual); 
-		else return null;
+		if (indexOfActual != -1)
+			return myWaves.get(indexOfActual);
+		else
+			return null;
 	}
 	
-	private static class Wave implements Comparable {
+	/**
+	 * Wave object that holds data about each wave in the set of waves. 
+	 * 
+	 * @author Nicholas A. Hays & Ethan Rowell
+	 */
+	private static class Wave implements Comparable<Wave> {
 		private int myStartTime;
 		private int myFunPts;
-		private int myWaitTime;
 		private int myEndTime;
 		private int myActualMaxFunPts;
 		private int myTotMaxFunPts;
-
+		
+		/**
+		 * Constructs the wave, and assigns values to each wave. 
+		 * @param startTime the start time the wave will crash on the shore.
+		 * @param funPoints the value assigned to each wave (i.e number representing height, power, etc.)
+		 * @param waitTime the duration of the wave plus the time it takes to swim back out to shore. 
+		 */
 		public Wave(int startTime, int funPoints, int waitTime) {
 			myStartTime = startTime;
 			myFunPts = funPoints;
-			myWaitTime = waitTime;
 			myEndTime = startTime + waitTime;
 		}
-
-		public boolean isOverlapping(Wave theOtherWave) {
-			if (this.myStartTime < theOtherWave.myEndTime) {
-				return true;
-			}
-			return false;
+		
+		/**
+		 * Sets the combined total fun points that can acutally be realized by taking this particular
+		 * wave.  
+		 * @param the actual combined maximum points that can be made taking this wave. 
+		 */
+		public void setActualMaxFunPoints(int points) {
+			myActualMaxFunPts = points;
 		}
 		
-		public void setActualMaxFunPoints(int funPoints) {
-			myActualMaxFunPts = funPoints;
-		}
-
-		public void setTotMaxFunPts(int funPoints) {
-			myTotMaxFunPts = funPoints;
-		}
-
-		public int getMyStartTime() {
-			return myStartTime;
-		}
-
-		public int getMyFunPoints() {
-			return myFunPts;
-		}
-
-		public int getMyWaitTime() {
-			return myWaitTime;
-		}
-
-		public int getMyEndTime() {
-			return myEndTime;
+		/**
+		 * Sets the combined total fun points that can be made up to this wave. This 
+		 * method may take into account previous wave selections that do not include this wave 
+		 * if they produce larger totals. 
+		 * 
+		 * @param funPoints the value assigned to each wave. 
+		 */
+		public void setTotMaxFunPts(int points) {
+			myTotMaxFunPts = points;
 		}
 
 		@Override
-		public int compareTo(Object theOtherWave) {
-			if (this.myStartTime < ((Wave) theOtherWave).myStartTime) {
+		public int compareTo(Wave theOtherWave) {
+			if (this.myStartTime < (theOtherWave).myStartTime) {
 				return -1;
 			} else {
 				return 1;
